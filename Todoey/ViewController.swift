9c4828269d1e3bd18a32af9f0a3a58eacd9e43e0
Cole Hayden <colehayden@coles-macbook-pro.local>
2019-01-14 00:07:39 -0600

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Cole Hayden on 1/14/19.
+//  Copyright © 2019 Cole Hayden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
